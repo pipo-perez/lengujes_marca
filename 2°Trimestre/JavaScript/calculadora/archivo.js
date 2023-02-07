119c8da8b8481b20a06c93c89c91a107b1ae8090
pipo-perez <andres.perez.dosa@gmail.com>
2023-02-07 20:08:19 +0100

    calculadora

--- 2°Trimestre/JavaScript/calculadora/archivo.js
+++ 2°Trimestre/JavaScript/calculadora/archivo.js
@@ -1,33 +1,31 @@
 //contiene la operacion 0 reslultado parcial
 let contenido = "";
 //elemento donde se colocan la operacion que se esta realizando
-let operRealizada = document. getElementById("operacionUno");
+let operRealizada = document.getElementById("operacionUno");
 //elemento donde se coloca el resultado
-let txtResul = document. getElementById("resultado");
+let txtResul = document.getElementById("resultado");
 //ultimo operado seleccionado
 let operadorSeleccionado = "";
 //numero que se va a ingresado
 let numero = "";
 
-//variable que se usa para determinar si lo ultimo numero presionada es un 0 
-let ultimoDigito = "" ;
+//variable que se usa para determinar si lo ultimo numero presionada es un 0
+let ultimoDigito = "";
 
 function operacion(digitosVarios) {
-    
-    numero = digitosVarios;
+  numero = digitosVarios;
 
-    ultimoDigito = "operacion";
-    contenido = contenido + digitosVarios;
-    // numero = 0;
-    operRealizada.innerHTML = contenido;
+  ultimoDigito = "operacion";
+  contenido = contenido + digitosVarios;
+  // numero = 0;
+  operRealizada.innerHTML = contenido;
 }
 
 // Llamar a los simbolos de la operación
 function operar(distintasOperaciones) {
-    
-    numero = distintasOperaciones;
-    ultimoDigito = "operar";
+  numero = distintasOperaciones;
+  ultimoDigito = "operar";
 
-    contenido = contenido + distintasOperaciones;
-    operRealizada.innerHTML = contenido;
+  contenido = contenido + distintasOperaciones;
+  operRealizada.innerHTML = contenido;
 }
