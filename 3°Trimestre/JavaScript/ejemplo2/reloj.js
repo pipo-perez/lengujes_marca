95592bea287e4cd0f644310fc707d49123d62a9c
MONTSERRAT12 <francisco.sanchez.dosa@gmail.com>
2023-03-14 14:01:16 +0100

    reloj js

--- 3°Trimestre/JavaScript/ejemplo2/reloj.js
+++ 3°Trimestre/JavaScript/ejemplo2/reloj.js
@@ -5,17 +5,17 @@ let segundos=0;
 // horas
 window.setInterval (function(){
     document.getElementById('hora').innerHTML = horas;
-    horas++;
+    // horas++;
 },1);
 
 // minutos
 window.setInterval (function(){
     document.getElementById('minutos').innerHTML = minutos;
-    minutos++;
+    // minutos++;
 },1000);
 
 // segundos
 window.setInterval (function(){
-    document.getElementById('reloj').innerHTML = segundos;
-    segundos++;
+    document.getElementById('segundo').innerHTML = segundos;
+    // segundos++;
 },10000);
