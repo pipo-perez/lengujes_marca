be83fe5fc6a9f742155192bd30ab367def37b75c
MONTSERRAT12 <francisco.sanchez.dosa@gmail.com>
2023-03-14 14:30:12 +0100

    trilero Bea

--- 3°Trimestre/JavaScript/trilero/ejemploBea/codigo.js
+++ 3°Trimestre/JavaScript/trilero/ejemploBea/codigo.js
@@ -1,6 +1,7 @@
 // variables
 let numAleatorios;
 let reloj;
+let imagenn="img";
 
 function aleatorios(min,max){
     return Math.floor((Math.random()*(max-min+1))+min);
@@ -14,4 +15,10 @@ window.onload = function cargaPrincipal(){
     numAleatorios=aleatorios(1,4);
     reloj=document.getElementById('reloj');
     reloj.innerHTML=numAleatorios;
+    DescubrirPelota(imagenn+numAleatorios);
+}
+
+// descubri pelota
+function DescubrirPelota(posicion) {
+    document.getElementById(posicion).src="pelota.jpeg"
 }
