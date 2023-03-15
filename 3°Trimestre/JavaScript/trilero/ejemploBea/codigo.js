de5dbd7d2de0a14578b61aac904f9d4143e89b1e
RichardANL <richard.nolasco.dosa@gmail.com>
2023-03-15 10:41:15 +0100

    trilero js Bea

--- 3°Trimestre/JavaScript/trilero/ejemploBea/codigo.js
+++ 3°Trimestre/JavaScript/trilero/ejemploBea/codigo.js
@@ -16,9 +16,20 @@ window.onload = function cargaPrincipal(){
     reloj=document.getElementById('reloj');
     reloj.innerHTML=numAleatorios;
     DescubrirPelota(imagenn+numAleatorios);
+    setTimeout(cubrirPelota,1000);
+    numAleatorios=aleatorios(1,4);
 }
 
 // descubri pelota
 function DescubrirPelota(posicion) {
     document.getElementById(posicion).src="pelota.jpeg"
+}
+
+// cubrir pelota
+function cubrirPelota(){
+    document.getElementById(img1).src="vaso.jpg";
+    document.getElementById(img2).src="vaso.jpg";
+    document.getElementById(img3).src="vaso.jpg";
+    document.getElementById(img4).src="vaso.jpg";
+
 }
