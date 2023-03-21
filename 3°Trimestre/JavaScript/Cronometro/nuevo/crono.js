80c502a98fc221c0917090f74bf5ea1a58b7c1bf
MONTSERRAT12 <francisco.sanchez.dosa@gmail.com>
2023-03-21 12:28:05 +0100

    cronometro

---
+++ 3°Trimestre/JavaScript/Cronometro/nuevo/crono.js
@@ -0,0 +1,37 @@
+// variables
+let minutos=0;
+let segundos=0;
+
+// funcion reloj
+function reloj() {
+    document.getElementById("reloj").innerHTML=minutos+":"+segundos;
+
+    if (segundos<=59) {
+        segundos++;
+           
+    }else{
+        minutos++;     
+        segundos=0;   
+    }
+}
+
+// parar
+function parar() {
+    clearInterval(id);
+}
+
+// reanudar
+function reanudar() {
+    id=setInterval(reloj,1000);    
+}
+
+// resetear
+function resetear() {
+    minutos=0;
+    segundos=0;
+}
+
+// inicio
+window.onload = function Carga(){
+    id=setInterval(reloj,1000);
+}
